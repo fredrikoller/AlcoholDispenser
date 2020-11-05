59ee9186e00df790afe9cd18ec0b78a551a7f6ab
Fredrik Öller <oller.fredrik@gmail.com>
2020-11-05 19:13:15 +0100

    order numbers descending

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace AlcoholDispenser
 {
@@ -6,7 +8,15 @@ namespace AlcoholDispenser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            var filePath = @"Vikter.txt";
+            float bagMaxWeight = 15.0f;
+            int bagCount;
+
+            List<string> fileContent = FileHandler.ReadFile(filePath);
+            List<float> listContent = fileContent
+                            .Select(x => float.Parse(x, CultureInfo.InvariantCulture.NumberFormat))
+                            .OrderByDescending(l => l)
+                            .ToList();
         }
     }
 }
